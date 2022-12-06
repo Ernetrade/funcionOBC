95074db993cc8fa64d241efe51ec8a40b4c72178
legiónDev <ernetrades@gmail.com>
2022-12-06 13:44:33 -0500

    Primer commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+    public static int suma(int a, int b, int c){
+        int resultado = a + b + c;
+        return resultado;
+    }
+}
